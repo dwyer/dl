3660348ebd845ca7d7e10d853429af9aae6b2f7f
Casey Dwyer <caseydwyer@gmail.com>
2017-02-27 20:41:31 -0800

    Use futures.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-#-*-coding:utf-8-*-
+# -*-coding:utf-8-*-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -17,5 +17,6 @@ setup(
     scripts=['dl'],
     install_requires=[
         'beautifulsoup4',
+        'futures',
     ],
 )
