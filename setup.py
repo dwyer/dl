56fb5f7a70a862133e111e76eee7bad3d1459ace
Casey Dwyer <caseydwyer@gmail.com>
2025-05-12 14:25:55 -0700

    Drop python2 support.

--- setup.py
+++ setup.py
@@ -1,14 +1,8 @@
-# -*-coding:utf-8-*-
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
-
 from setuptools import setup
 
 setup(
     name='dl',
-    version='0.1',
+    version='0.2-dev',
     description='A command-line downloader.',
     author='Casey Dwyer',
     author_email='caseydwyer@gmail.com',
